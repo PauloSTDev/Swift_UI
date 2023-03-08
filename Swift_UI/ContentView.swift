1b5dc466a3d758c7e0b7aaf030a581943425c33f
Paulo Santos <compasso-paulo.santos@ailos.coop.br>
2023-03-08 08:55:00 -0300

    Initial Commit

---
+++ Swift_UI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Swift_UI
+//
+//  Created by Paulo Afonso Della Mêa dos Santos on 08/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift_UI/Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UIApp.swift
+//  Swift_UI
+//
+//  Created by Paulo Afonso Della Mêa dos Santos on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
