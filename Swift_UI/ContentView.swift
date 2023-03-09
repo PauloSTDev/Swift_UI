9ea0f9e28f8ae53b7ca69859dd2c28002884bac3
Paulo Santos <compasso-paulo.santos@ailos.coop.br>
2023-03-09 15:18:25 -0300

    Add VStack, background colors, padding e edgesIgnoringSafeArea

--- Swift_UI/ContentView.swift
+++ Swift_UI/ContentView.swift
@@ -2,20 +2,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            Text("SwiftUI")
-                .foregroundColor(Color.black)
-            Text("SWIFT").foregroundColor(Color.black)
-            Text("UI").foregroundColor(Color.black)
-            List {
-                Text("Item 1")
-                Text("Item 2")
-                Text("Item 3")
-                Text("Item 4")
-                Text("Item 5")
+        
+        GeometryReader{ view in
+            
+            // Principal VStack
+            VStack{
+                // Header VStack
+                VStack{
+                    Text("SwiftUI")
+                        .foregroundColor(Color.black)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 60)
+                    Text("SWIFT").foregroundColor(Color.black)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("UI").foregroundColor(Color.black)
+                        .font(.custom("Avenir Black", size: 24))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.orange)
+                // Ignore SafeArea
+                .edgesIgnoringSafeArea(.all)
+                List {
+                    Text("Item 1")
+                    Text("Item 2")
+                    Text("Item 3")
+                    Text("Item 4")
+                    Text("Item 5")
+                }
             }
+            
         }
-        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
     }
 }
 
